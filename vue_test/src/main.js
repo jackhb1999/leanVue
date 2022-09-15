05c1eaec70a7328dff9ccd7f13ac762073459e34
fanjixiang <jack.h.b.fan@outlook.com>
2022-09-15 18:04:27 +0800

    到求和案例

--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
+// 引入插件
+import vueResource from 'vue-resource'
 
 Vue.config.productionTip = false
 
+// 使用插件
+Vue.use(vueResource)
 
 
 new Vue({
